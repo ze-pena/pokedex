915c30fbed1930171444636610ed8f0e72d59f88
zombiePumpkin <ze__augusto@hotmail.com>
2022-05-29 03:48:25 -0300

    Add PokeCard component

---
+++ src/components/PokeCard/PokeCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <li class="pk-card">
+    <div class="pk-card__content">
+      <picture class="pk-content__img">
+        <img src="" />
+      </picture>
+      <div class="pk-content__info">
+        <div class="pk-info__name">
+          <span>Charizard</span>
+        </div>
+        <div class="pk-info__id">
+          <span>#0089</span>
+        </div>
+      </div>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'PokeCard',
+}
+</script>
+
+<style lang="scss" scoped src="./PokeCard.scss" />
