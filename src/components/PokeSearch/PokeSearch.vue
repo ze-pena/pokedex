1dfbadfced10e94dc2d05d9742850ba654e782d6
zombiePumpkin <ze__augusto@hotmail.com>
2022-05-29 03:49:27 -0300

    Add PokeSearch component

---
+++ src/components/PokeSearch/PokeSearch.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="pk-search">
+    <input class="pk-search__input" type="text" placeholder="Search" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PokeSearch',
+}
+</script>
+
+<style lang="scss" scoped src="./PokeSearch.scss" />
