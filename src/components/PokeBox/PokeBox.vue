f739e506ca4b33d2c94d3dcba881a867c7b85b55
zombiePumpkin <ze__augusto@hotmail.com>
2022-05-29 03:47:23 -0300

    Add PokeBox component

---
+++ src/components/PokeBox/PokeBox.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="pk-box">
+    <ul class="pk-box__container">
+      <slot name="poke-cards" />
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PokeBox',
+}
+</script>
+
+<style lang="scss" scoped src="./PokeBox.scss" />
