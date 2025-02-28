08c0ee9ad36762b1b7d4c866743801a4e8dee65a
ze-pena <jose.pena.dev@gmail.com>
2025-02-27 23:43:31 -0300

    feat: create "Body Content" component

---
+++ src/components/BodyContent/BodyContent.svelte
@@ -0,0 +1,36 @@
+<script>
+  // Components
+  import PokemonCard from "../PokemonCard/PokemonCard.svelte"
+  const { pokemonList } = $props()
+</script>
+
+<div class="body-content">
+  <ul>
+    {#each pokemonList as pokemon}
+      <PokemonCard {pokemon} />
+    {/each}
+  </ul>
+</div>
+
+<style lang="postcss">
+.body-content {
+  flex-grow: 1;
+  height: 100%;
+  overflow: hidden;
+  border-radius: 8px;
+  border: 1px solid var(--plastic-color-2);
+  background-color: var(--neutral-color-2);
+  
+  ul {
+    flex-grow: 1;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: flex-start;
+    justify-content: space-evenly;
+    padding: 32px 24px;
+    gap: 32px 24px;
+    height: 100%;
+    overflow: hidden scroll;
+  }
+}
+</style>
