d07c304386f644601cb9157c966076df87169255
ze-pena <jose.pena.dev@gmail.com>
2025-02-27 23:47:22 -0300

    feat: create "Pokedex Layout" component

---
+++ src/layouts/PokedexLayout/PokedexLayout.svelte
@@ -0,0 +1,18 @@
+<script>
+  const { pokemonList, header, body } = $props();
+</script>
+
+<div class="pokedex-layout">
+  {@render header()}
+  {@render body(pokemonList)}
+</div>
+
+<style lang="postcss">
+.pokedex-layout {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  background-color: var(--plastic-color-1);
+}
+</style>
