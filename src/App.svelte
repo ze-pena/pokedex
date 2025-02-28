ef4eaf7969dd48e3f43a0f1e7903f6859ba7ef07
ze-pena <jose.pena.dev@gmail.com>
2025-02-28 12:15:24 -0300

    refactor: use previous pokedex services created to set the pokemon list

--- src/App.svelte
+++ src/App.svelte
@@ -4,41 +4,28 @@
   import PokedexHeader from "./components/PokedexHeader/PokedexHeader.svelte";
   import PokedexBody from "./components/PokedexBody/PokedexBody.svelte";
   // Store
-  import { pokedex } from "./store/pokedex.svelte.js"
+  import { pokedexStore } from "./store/pokedex.svelte.js"
+  // Serviços
+  import pokedexService from "./services/pokedex"
+  // Utilidades
+  import { sortById } from "./utils/sort";
+  import { extractPokemonIdFromURL } from "./utils/pokedex";
 
-  const baseURL = "https://pokeapi.co/api/v2";
-
-  const getPokemonGeneration = async (generationId) => {
-    const requestURL = `${baseURL}/generation/${generationId}`;
-
+  const setPokemonList = async (generationId) => {
     try {
-      const response = await fetch(requestURL);
-      const responseJSON = await response.json();
-      return responseJSON;
-    } catch (error) {
+      const { pokemon_species } = await pokedexService.getGenerations(generationId);
+      const pokemonList = await Promise.all(pokemon_species.map(specie => {
+        const id = extractPokemonIdFromURL(specie.url);
+        return pokedexService.getPokemon(id);
+      }));
+      pokedexStore.pokemonList = sortById(pokemonList);
+    } catch(error) {
+      pokedexStore.pokemonList = [];
       console.log(error);
     }
   };
 
-  const getPokemons = async (generationId) => {
-    const pokemonGeneration = await getPokemonGeneration(generationId);
-    const pokemonList = pokemonGeneration.pokemon_species;
-
-    try {
-      const responseList = await Promise.all(pokemonList.map(pokemon => fetch(`${baseURL}/pokemon/${pokemon.name}`)));
-      const responseListJSON = await Promise.all(responseList.map(response => response.json()));
-      return responseListJSON.sort((a, b) => a.id > b.id ? 1 : -1);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const configurePokemonList = async (generationId) => {
-    const pokemonList = await getPokemons(generationId);
-    pokedex.pokemonList = pokemonList;
-  };
-
-  configurePokemonList(pokedex.generation);
+  setPokemonList(pokedexStore.generation);
 </script>
 
 <PokedexLayout>
