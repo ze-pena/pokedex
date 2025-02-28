f29ad0ee5954abdfcbb085fd9cf226c617fbf9db
ze-pena <jose.pena.dev@gmail.com>
2025-02-27 23:40:08 -0300

    feat: create "Header Search" component

---
+++ src/components/HeaderSearch/HeaderSearch.svelte
@@ -0,0 +1,35 @@
+<script>
+  // Shared States
+  import { pokemon } from "../../shared/pokedexState.svelte.js"
+</script>
+
+<div class="header-search">
+  <input type="text" title="pesquisar" bind:value={pokemon.nameFilter}/>
+</div>
+
+<style lang="postcss">
+.header-search {
+  flex-grow: 1;
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-start;
+  padding: 0 16px 8px 16px;
+  border-radius: 0 8px 0 0;
+  border-width: 6px 1px 0 0;
+  border-style: solid;
+  border-color: var(--plastic-color-2);
+
+  input {
+    width: 100%;
+    max-width: 280px;
+    height: 45px;
+    padding: 8px 16px;
+    font-size: 1.45rem;
+    color: var(--screen-color-2);
+    outline: none;
+    border-radius: 4px;
+    border: 1px solid var(--plastic-color-2);
+    background-color: var(--screen-color-1);
+  }
+}
+</style>
